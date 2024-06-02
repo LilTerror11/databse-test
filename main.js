20e41fedc3c43cb15e15673c509bc473263d1ab4
LilTerror11 <144447975+LilTerror11@users.noreply.github.com>
2024-06-02 15:30:55 +0930

    More Testing

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 console.log("Main.js is working!");
 
-console.log("${{vars.TEST}}");
-console.log("${{vars.TEST2}}");
+require('dotenv').config();
+
+let TEST = process.env.TEST;
+let TEST2 = process.env.TEST2;
+
+
+console.log(TEST);
+console.log(TEST2);
